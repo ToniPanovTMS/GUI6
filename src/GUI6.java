a3cc221fea2ab4bc389bce416aba7503a9a74010
ToniPanovTMS <-l>
2023-07-31 15:33:18 +0300

    перемещение 3

--- src/GUI6.java
+++ src/GUI6.java
@@ -1,13 +1,12 @@
 import java.awt.*;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.net.URL;
 import javax.imageio.ImageIO;
 import javax.swing.*;
+
 
 public class GUI6 {
     static JFrame frame = new JFrame();
@@ -15,41 +14,61 @@ public class GUI6 {
     static int dx = 0, dy = 0;
 
     static public void move(KeyEvent e) {//метод движения по клавиатуре
-        if (e.getKeyCode() == KeyEvent.VK_LEFT) {
-            dx -= 20;
-            if (dx < 0) {
+        if (e.getKeyCode() == KeyEvent.VK_LEFT && e.isShiftDown()==true) {
+            dx -= 40;
+            if (dx < -50) {
                 dx = 1000;
             }
             l.setLocation(dx, dy);
-            System.out.println(dx + " " + dy);
 
+        }else if(e.getKeyCode() == KeyEvent.VK_LEFT){
+            dx -= 20;
+            if (dx < -50) {
+                dx = 1000;
+            }
+            l.setLocation(dx, dy);
         }
 
-        if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+        if (e.getKeyCode() == KeyEvent.VK_RIGHT&& e.isShiftDown()==true) {
+            dx += 40;
+            if (dx > 1000) {
+                dx = -50;
+            }
+            l.setLocation(dx, dy);
+        }else if(e.getKeyCode() == KeyEvent.VK_RIGHT){
             dx += 20;
             if (dx > 1000) {
                 dx = -50;
             }
             l.setLocation(dx, dy);
-            System.out.println(dx + " " + dy);
         }
 
-        if (e.getKeyCode() == KeyEvent.VK_UP) {
+        if (e.getKeyCode() == KeyEvent.VK_UP&& e.isShiftDown()==true) {
+            dy -= 40;
+            if (dy < -50) {
+                dy = 1050;
+            }
+            l.setLocation(dx, dy);
+        }else if(e.getKeyCode() == KeyEvent.VK_UP){
             dy -= 20;
-            if (dy < 0) {
+            if (dy < -50) {
                 dy = 1000;
             }
             l.setLocation(dx, dy);
-            System.out.println(dx + " " + dy);
         }
 
-        if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+        if (e.getKeyCode() == KeyEvent.VK_DOWN&& e.isShiftDown()==true) {
+            dy += 40;
+            if (dy > 1000) {
+                dy = -50;
+            }
+            l.setLocation(dx, dy);
+        }else if(e.getKeyCode() == KeyEvent.VK_DOWN){
             dy += 20;
             if (dy > 1000) {
                 dy = -50;
             }
             l.setLocation(dx, dy);
-            System.out.println(dx + " " + dy);
         }
     }
 
@@ -59,9 +78,9 @@ public class GUI6 {
         Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
         frame.setBounds(dim.width / 2 - 1000 / 2, dim.height / 2 - 1000 / 2, 1000, 1000);
         BufferedImage im = ImageIO.read(new File("src/Son.png"));
-        JPanel panel = new JPanel (new FlowLayout(FlowLayout.LEFT));
+        JPanel panel = new JPanel(new FlowLayout(FlowLayout.LEFT));
         panel.setFocusable(true);
-        l = new JLabel(new ImageIcon(im),JLabel.RIGHT);
+        l = new JLabel(new ImageIcon(im), JLabel.RIGHT);
         panel.add(l, BorderLayout.NORTH);
         frame.add(panel);
         panel.addKeyListener(new KeyAdapter() {
@@ -72,81 +91,3 @@ public class GUI6 {
         frame.setVisible(true);//делаем форму видимой
     }
 }
-
-
-/*import javax.imageio.ImageIO;
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-import java.net.URL;
-
-public class T330004 {
-    static JFrame frame = new JFrame();//создаем форму
-    static JLabel l;//объект с картинкой
-    static int dx=0, dy=0;//задаем размер окна
-
-
-    static public void move(KeyEvent e) {//метод движения по клавиатуре
-        if (e.getKeyCode() == KeyEvent.VK_LEFT) {
-            dx -= 20;
-            if (dx < 0) {
-                dx = 0;
-            }
-            l.setLocation(dx, dy);
-            System.out.println(dx + " " + dy);
-
-        }
-
-        if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
-            dx += 20;
-            if (dx > 1000) {
-                dx = 1000;
-            }
-            l.setLocation(dx, dy);
-            System.out.println(dx + " " + dy);
-        }
-
-        if (e.getKeyCode() == KeyEvent.VK_UP) {
-            dy -= 20;
-            if (dy < 0) {
-                dy = 0;
-            }
-            l.setLocation(dx, dy);
-            System.out.println(dx + " " + dy);
-        }
-
-        if (e.getKeyCode() == KeyEvent.VK_DOWN) {
-            dy += 20;
-            if (dy > 1000) {
-                dy = 1000;
-            }
-            l.setLocation(dx, dy);
-            System.out.println(dx + " " + dy);
-        }
-    }
-
-    public static void main(String[] args) throws IOException {
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setTitle("Простое перемещение клавиатурой");
-        Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
-        frame.setBounds(dim.width / 2 - 1000 / 2, dim.height / 2 - 1000 / 2, 1000, 1000);
-        BufferedImage im = ImageIO.read(new File("src/Son.png"));
-        JPanel panel = new JPanel (new FlowLayout(FlowLayout.LEFT));
-        panel.setFocusable(true);
-        l = new JLabel(new ImageIcon(im),JLabel.RIGHT);
-        panel.add(l, BorderLayout.NORTH);
-        frame.add(panel);
-        panel.addKeyListener(new KeyAdapter() {
-            public void keyReleased(KeyEvent e) {
-                move(e);
-            }
-        });
-        frame.setVisible(true);//делаем форму видимой
-    }
-}
-
- */
